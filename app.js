23f1bdfae3ed34bc191e03edde2ab4d955821567
HildaAb <ilona.aase@gmail.com>
2025-02-18 12:56:18 +0100

    added basic HTML and css and created the questions

---
+++ app.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "How old was Tom Cruise in 2024?",
+    a: "20",
+    b: "35",
+    c: "60",
+    d: "110",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programing language in 2025?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "Who is the president of US?",
+    a: "Ronald Regan",
+    b: "Donald Trump",
+    c: "Dolly Parton",
+    d: "Cher",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notification",
+    d: "Helicopters Terminal Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "1993",
+    correct: "b",
+  },
+];
